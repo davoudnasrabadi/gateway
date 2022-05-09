2f5bba8ac6f650cf007584e9ac1a91e586d4429f
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-09 10:58:37 +0430

    interfaces added

---
+++ src/interfaces.ts
@@ -0,0 +1,32 @@
+import { Observable, from } from 'rxjs';
+
+export interface getById {
+  id: string
+}
+
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+
+}
+export interface DeleteMsg {
+  msg: string;
+}
+export interface UserCreateDto {
+  username: string;
+  email: string;
+  password: string;
+}
+export interface UpdateMsg {
+  msg: string;
+}
+export interface UsersService {
+  findOne(id: getById): User;
+  findAll(id: getById): string;
+  createOne(body: UserCreateDto): User[];
+  DeleteOne(id: getById): DeleteMsg;
+  UpdateOne(body: User): UpdateMsg
+}
