e5081e7ab933d797c241423efba2388fcebff100
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-09 11:01:22 +0430

    app service added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {UsersService} from './interfaces'; 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private userService: UsersService;
+  constructor(
+    @Inject('USER_SERVICE') private userServiceClient: ClientGrpc
+  ) {}
+  onModuleInit() {
+    this.userService = this.userServiceClient.getService<UsersService>('UsersService');
+  }
+  async getById(id:string) {
+    if(id!= 'all'){
+    const data = {id:id}
+    return this.userService.findOne(data);
+    }
+    else {
+      const data= {id:'all'}
+      return this.userService.findAll(data);
+    
+    }
+    
+  }
+  deleteById(id:string) {
+    const data = {id:id}
+    return this.userService.DeleteOne(data);
+  }
+  getAll(data:any) {
+    return this.userService.findAll(data);
+  }
+  createOne(body:any){
+    return this.userService.createOne(body);
+  }
+
+  updateOne(id:string,body:any){
+       const user = {
+         id:id,
+         username:body.username,
+         email:body.email,
+         password:body.password
+       }
+      return this.userService.UpdateOne(user);
   }
 }
