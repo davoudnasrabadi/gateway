55684b9d566ea6e404d0b0c4a0780ff06d25be93
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-09 10:59:54 +0430

    app module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+    {
+      name: 'USER_SERVICE',
+      transport: Transport.GRPC,
+      options: {
+        package: 'users',
+        protoPath: join(__dirname, '../src/users.proto'),
+      },
+    }
+  ]),],
   controllers: [AppController],
   providers: [AppService],
 })
