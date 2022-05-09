8bc4d1ea237477a88de7a7085ba5adf343d1cf56
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-09 11:05:04 +0430

    dockerfile added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post,Body, Delete, Put } from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller()
+import {ValidationPipe} from './myPipe';
+import {CreateUserDto} from './Dtos/createDto';
+import { identifier } from '@babel/types';
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getById(@Param('id') id:string) {
+    return this.appService.getById(id);
   }
+
+  @Get('/all')
+  async getAll(@Param('id') id:string){
+    return await this.appService.getAll("all");
+  }
+
+  @Post()
+  createOne(@Body(new ValidationPipe()) body:CreateUserDto){
+    return this.appService.createOne(body);
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id:string){
+        return this.appService.deleteById(id);
+  }
+  @Put(':id')
+   updateById(@Param('id') id,@Body(new ValidationPipe()) body:CreateUserDto){
+     return this.appService.updateOne(id,body);
+   }
 }
