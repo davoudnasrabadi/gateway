b83963d185668f5fc956cdf98b2ec65896ca6f5d
davoudnasrabadi <davoudnsr90@gmail.com>
2022-05-11 15:42:31 +0430

    logger added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post,Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Param, Post,Body, Delete, Put, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 import {ValidationPipe} from './myPipe';
 import {CreateUserDto} from './Dtos/createDto';
@@ -8,7 +8,7 @@ import { tokenDto } from './Dtos/response.dto';
 @Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
+  private readonly logger = new Logger(AppController.name);
   @Get(':id')
   getById(@Param('id') id:string):Promise<UserByIdDto | errorMsg>  {
     return this.appService.getById(id);
@@ -16,23 +16,28 @@ export class AppController {
 
   @Get('')
   async getAll():Promise<AllMsg>{
+    this.logger.log('Gw request for getting all users');
     return await this.appService.getAll();
   }
 
   @Post('/register')
   createOne(@Body(new ValidationPipe()) body:CreateUserDto):string{
+    this.logger.log('Gw request for registering a user');
     return this.appService.register(body);
   }
   @Post('/login')
   login(@Body(new ValidationPipe()) body:CreateUserDto):string{
+    this.logger.log('Gw request for login a user');
     return this.appService.login(body);
   }
   @Delete(':id')
   deleteById(@Param('id') id:string):DeleteMsg{
+    this.logger.log('Gw request for deleting a user');
         return this.appService.deleteById(id);
   }
   @Put(':id')
    updateById(@Param('id') id,@Body(new ValidationPipe()) body:CreateUserDto):UpdateMsg{
+    this.logger.log('Gw request for updating a user');
      return this.appService.updateOne(id,body);
    }
 }
